c353a0b27293432c606032606598143eba7469bd
Edward Pena Pro <Eduardo.Pena@sryas.com>
2022-11-14 13:21:54 -0600

    commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <userName />
+  </div>
+</template>
+
+<script>
+import userName from "./components/userName.vue";
+
+export default {
+  name: 'App',
+  components: {
+    userName
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+
+</style>
---
+++ src/components/userName.vue
@@ -0,0 +1,82 @@
+<template>
+
+<div>
+    <div class="masterStyle">
+<h1>Search teamates</h1>
+<input type="text" v-model="input">
+
+<div class="estilo">
+    <ul>
+    <li v-for="(name,index) in filteredNames" v-bind:key="index">
+        
+        {{name}}
+        
+    </li>
+</ul>
+</div>
+
+
+</div>
+</div>
+</template>
+
+<script>
+export default {
+name: "userName",
+data() {
+    
+    return {
+        names:['Parth','Eduard','Frank','Edger','Max','Chencho','Demian','Alexis','Citalali','Luz','Juan'],
+        
+        input:""
+    }
+},
+computed:{
+    filteredNames(){
+        return this.input !== '' ? this.names.filter(name=>name.toLowerCase().includes(this.input.toLowerCase())):''
+    }
+}
+}
+</script>
+
+<style scoped>
+
+.masterStyle{
+    
+    align-content: center;
+    border-radius: 10%;
+    box-shadow: 10px 5px 5px black;
+}
+h1{
+  font-weight: bold;
+  font-size: 30;
+  border-radius: 10%;
+}
+input{
+  border: 10%;
+  border-width: 1px;
+ 
+}
+.estilo{
+    
+    flex-wrap: wrap;
+    width: 400px;
+    border-radius: 10%;
+    background-color: #eee;
+    border-color: black;
+
+}
+ul{
+    background-color: #eee;
+    margin: 1px;
+    border-radius: 10%;
+    
+}
+li{
+    padding: auto;
+    font-weight: bold;
+    text-shadow: 1px 1px 2px black;
+}
+    
+</style>
+
